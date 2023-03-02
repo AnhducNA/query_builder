328708704672cd7fb23aff73f698bd99a416821b
AnhducNA <Anhduc05022001>
2023-03-02 15:19:44 +0700

    query builder version 2

--- test/index.php
+++ test/index.php
@@ -5,7 +5,7 @@ require 'vendor/autoload.php';
 $config = ['DB_SERVERNAME' => '127.0.0.1', 'DB_PORT' => '3306', 'DB_DATABASE' => 'query_builder', 'DB_USERNAME' => 'root', 'DB_PASSWORD' => 'password'];
 $connection = new \Anhduc\QueryBuilder\Connection\Connection($config);
 
-//$builder = (new Anhduc\QueryBuilder\DB())->select('id', 'name')->table('users')->where('id', '=', 1)->all();
+//$builder = (new Anhduc\QueryBuilder\DB())->select('id', 'name')->table('users')->where('id', '=', 1)->get();
 //dd($builder);
 
 //$user = \Anhduc\QueryBuilder\Models\User::where('id', '=', '1')->first();
